35aad3a41e9c7acaca53588c053ce278197ce70d
Joan <92529728+CyberNFS@users.noreply.github.com>
2022-07-24 20:39:00 +0100

    Added cpu usage check with psutil.

--- system_check.py
+++ system_check.py
@@ -3,6 +3,8 @@ import shutil
 import os
 import sys
 import socket
+import psutil
+
 
 def check_reboot():
   """Returns True if the computer has pending reboot."""
@@ -25,18 +27,24 @@ def check_root_full():
   """Returns True is the root partition is full, False otherwise."""
   return check_disk_full(disk='/', min_gb=2, min_percent=10)
 
-  def check_no_network():
-    """Returns True if it fails to resolve Google's URL, False otherwise."""
-    try:
-      socket.gethostbyname("www.google.com")
-      return False
-    except:
-      return True
+
+def check_no_network():
+  """Returns True if it fails to resolve Google's URL, False otherwise."""
+  try:
+    socket.gethostbyname("www.google.com")
+    return False
+  except:
+    return True
+
+
+def check_cpu_constrained():
+  """Returns True if the cpu is having too much usage, False otherwise."""
+  return psutil.cpu_percent(1) > 75
 
 
 def main():
   checks = [(check_reboot, "Pending Reboot."),
-            (check_root_full, "Root partition full."), (check_no_network, "No working network.")]
+            (check_root_full, "Root partition full."), (check_no_network, "No working network."), (check_cpu_constrained, "CPU load too high.")]
   everything_ok = True
   for check, msg in checks:
     if check():
